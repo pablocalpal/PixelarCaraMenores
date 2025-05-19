e1d7d2e3f341d0cb3ab6368d9a810b5208fad138
Pablo <158524937+pablocalpal@users.noreply.github.com>
2025-05-19 16:45:41 +0200

    entrenamiento de modelo

--- Dockers/Bounding/codigo/test.py
+++ Dockers/Bounding/codigo/test.py
@@ -1,10 +1,29 @@
 from retinaface import RetinaFace
-resp = RetinaFace.detect_faces("img1.jpg")
-
-print(resp)
-
+import cv2
 import matplotlib.pyplot as plt
-faces = RetinaFace.extract_faces(img_path = "img1.jpg", align = True)
+
+# Cargar la imagen
+img_path = "img2.jpg"
+img = cv2.imread(img_path)
+
+# Detectar las caras
+resp = RetinaFace.detect_faces(img_path)
+
+# Dibujar los cuadrados alrededor de las caras detectadas
+for key in resp:
+    face = resp[key]
+    facial_area = face["facial_area"]
+    x1, y1, x2, y2 = facial_area
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+# Mostrar la imagen original con los cuadrados
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.axis("off")
+# Extraer y mostrar las caras
+faces = RetinaFace.extract_faces(img_path=img_path, align=True)
+plt.show()
+
 for face in faces:
-  plt.imshow(face)
-  plt.show()
+    plt.imshow(face)
+    plt.axis("off")
+    plt.show()
